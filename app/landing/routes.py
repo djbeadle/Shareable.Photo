67933a92a45a7f30c555935ae1913cbddbb50d22
Daniel Beadle <djbeadle@gmail.com>
2022-01-22 12:11:27 -0500

    Move S3 authorizations from Landing to Auth

--- app/landing/routes.py
+++ app/landing/routes.py
@@ -1,15 +1,9 @@
-from uuid import UUID
 from flask import render_template, request, Response, session, redirect, url_for, current_app
-from db_operations import create_event, get_event_info, list_users_events, record_upload, event_asset_count, get_images, get_next_asset_id
+from db_operations import create_event, get_event_info, list_users_events, event_asset_count, get_images
 from app.landing import landing_bp
 from app.toolbox import requires_auth
 
-
-import json, urllib
-from datetime import datetime
-
-
-from s3 import generate_presigned_post, create_presigned_url
+from s3 import create_presigned_url
 
 
 @landing_bp.context_processor
@@ -79,104 +73,3 @@ def get_event(user_facing_id: str):
         images=event_images,
         content=get_event_info(user_facing_id)
     )
-
-
-@landing_bp.route('/s3_upload_callback', methods = ['GET', 'POST', 'PUT'])
-def sns():
-    # TODO-Daniel: Verify Signature from Amazon to prevent malicious
-    """
-       [
-            {
-                "eventVersion" : "2.1",
-                "eventSource" : "aws:s3",
-                "awsRegion" : "us-east-1",
-                "eventTime" : "2022-01-12T18:13:40.260Z",
-                "eventName" : "ObjectCreated:Post",
-                "userIdentity" : {
-                    "principalId" : "AWS:AIDAUZYMYSEH2T565DTFL"
-                },
-                "requestParameters" : {
-                     "sourceIPAddress" : "151.205.187.56"
-                },
-                "responseElements" : {
-                    "x-amz-request-id" : "Q632NM5SXJZE1DZY",
-                    "x-amz-id-2" : "ygP97z1gE22xNh57jCt6ypnlEMi8Ab3PbiwAh+wO9TKQpCDCRdLk1et/7+C3L4vphMxV8Pr9rRwUuWP0BG1Nrq/NYPmyRjFy"
-                },
-                "s3" : {
-                    "s3SchemaVersion" : "1.0",
-                    "configurationId" : "Eventfire Upload",
-                    "bucket" : {
-                        "name" : "eventfire",
-                        "ownerIdentity" : {
-                        "principalId" : "A1N3DD51J9UNG7"
-                        },
-                        "arn" : "arn:aws:s3:::eventfire"
-                    },
-                    "object" : {
-                        "key" : "96bf309f-e2db-4910-8e74-96580a2e0c4b/IMG_2605.jpeg",
-                        "size" : 480277,
-                        "eTag" : "47486c8fe6dc435934dfd323da7beaa5",
-                        "sequencer" : "0061DF1A541D57A6A0"
-                    }
-                }
-            }
-        ]
-    """
-    # AWS sends JSON with text/plain mimetype
-    # TODO calculate e-tags client side and prevent duplicate uploads https://teppen.io/2018/06/23/aws_s3_etags/#what-is-an-s3-etag
-    try:
-        js = json.loads(request.data)
-    except:
-        pass
-
-    hdr = request.headers.get('X-Amz-Sns-Message-Type')
-    # subscribe to the SNS topic
-    if hdr == 'SubscriptionConfirmation' and 'SubscribeURL' in js:
-        # r = requests.get(js['SubscribeURL'])
-        with urllib.request.urlopen(js['SubscribeURL']) as f:
-            print(f.read().decode('utf-8'))
-
-    # if hdr == 'Notification':
-    #    print(js['Message'], js['Timestamp'])
-
-    msg = js['Message']
-    for r in json.loads(msg)['Records']:
-        # print(json.dumps(r, indent=2))
-        folder, filename = r['s3']['object']['key'].split('/')
-        record_upload(
-            filename,
-            folder,
-            r['eventTime'],
-            r['awsRegion'],
-            r['requestParameters']['sourceIPAddress'],
-            r['s3']['object']['size'],
-            r['s3']['object']['eTag'],
-        )
-
-    return 'OK\n'
-
-
-@landing_bp.route('/<user_facing_id>/s3/params')
-def get_presigned_s3_upload_url(user_facing_id):
-    # https://github.com/transloadit/uppy/blob/main/packages/%40uppy/companion/src/server/controllers/s3.js
-    # TODO-prod: Keep tracing of the user_facing_ids and validate if this is in the database. For now just see if it's a valid UUID
-    try:
-        current_user_facing_id = UUID(user_facing_id)
-        print(f'metadata: {json.dumps(request.args)}')
-        uploader_name = request.args.get('metadata[uploader-name]')
-        file_type = request.args.get('type')
-    except ValueError as e:
-        print(e)
-        return Response({"error": "Now just hold on a minute, bucko."}, status=400, mimetype="application/json")
-
-    params = request.args
-    # Due to issues with how S3 encodes plus signs I'm just going to replace them with spaces for now.
-    filename_with_folder = f'{user_facing_id}/{get_next_asset_id(user_facing_id)[0]}_{uploader_name.replace(" ", "-")}_{params["filename"].replace("+", " ")}'
-    
-    fields = {
-        'x-amz-meta-uploader-name': uploader_name
-    }
-
-    x = generate_presigned_post(filename_with_folder, params['type'], fields, uploader_name)
-    # x['fields']['content_type'] = file_type
-    return json.dumps(x)
